1ce9d849f40f10196cb055be46a2c1cdc9e5666d
Trugamr <trugamr@live.in>
2023-09-25 23:17:50 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "hello"
+}
